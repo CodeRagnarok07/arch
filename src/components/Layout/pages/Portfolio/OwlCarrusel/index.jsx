251e94ecd67eb126259b6baff733544354c89b9d
ragnarok <angelriera1796@gmail.com>
2023-10-09 12:40:07 -0400

    fiexd OwlCarrusel all feactures

--- src/components/Layout/pages/Portfolio/OwlCarrusel/index.jsx
+++ src/components/Layout/pages/Portfolio/OwlCarrusel/index.jsx
@@ -16,14 +16,17 @@ export default function OwlCarrusel() {
     const refCarusel = useRef()
     const carrousel = refCarusel.current
 
+    const [current, setCurrent] = useState(1)
+
     const handleClick = (e, k) => {
         for (let index = 0; index < carrousel.children.length; index++) {
             const element = carrousel.children[index];
             element.className = styles.item
         }
 
+        setCurrent(k)
+
         if (refCarusel?.current?.children[0]) {
-            console.log(k - 1, k + 1);
             carrousel.children[k - 1]?.classList.add(styles.left)
             carrousel.children[k]?.classList.add(styles.main)
             carrousel.children[k + 1]?.classList.add(styles.right)
@@ -38,41 +41,106 @@ export default function OwlCarrusel() {
     useEffect(() => {
         if (refCarusel?.current?.children[0]) {
             const carrousel = refCarusel.current
-            carrousel.children[0]?.classList.add(styles.right)
-            carrousel.children[1]?.classList.add(styles.main)
-            carrousel.children[2]?.classList.add(styles.left)
+            carrousel.children[current - 1]?.classList.add(styles.right)
+            carrousel.children[current]?.classList.add(styles.main)
+            carrousel.children[current + 1]?.classList.add(styles.left)
         }
 
     }, [])
 
 
-    const [touchStart,setTouchStart] = useState()
+    const hanldeNext = (bool, actual) => {
 
+        carrousel.children[current]
+        for (let index = 0; index < carrousel.children.length; index++) {
+            const element = carrousel.children[index];
+            element.className = styles.item
+        }
+
+
+        if (refCarusel?.current?.children[0]) {
+
+            if (bool) {
+
+                carrousel.children[actual + 1]?.classList.add(styles.right)
+                carrousel.children[actual]?.classList.add(styles.main)
+                carrousel.children[actual - 1]?.classList.add(styles.left)
+
+                if (actual >= carrousel.children.length-1) {
+                    carrousel.children[0]?.classList.add(styles.right)
+                    setCurrent(-1)
+                }
+
+                if (actual <= 0) {
+                    carrousel.children[carrousel.children.length - 1]?.classList.add(styles.left)
+
+                } 
+
+            } else {
+                carrousel.children[actual+ 1]?.classList.add(styles.right)
+                carrousel.children[actual]?.classList.add(styles.main)
+                carrousel.children[actual- 1]?.classList.add(styles.left)
+
+                if (actual == 0) {
+                    carrousel.children[actual]?.classList.add(styles.main)
+                    carrousel.children[carrousel.children.length - 1]?.classList.add(styles.left)
+                } else {
+
+                }
+
+                if(actual < 0){
+                    carrousel.children[carrousel.children.length - 2]?.classList.add(styles.left)
+                    carrousel.children[carrousel.children.length - 1]?.classList.add(styles.main)
+                    setCurrent(-1)
+
+                }
+
+                // if (actual== carrousel.children.length - 1) {
+                //     carrousel.children[carrousel.children.length - 1]?.classList.add(styles.right)
+                //     setCurrent(0)
+                // }
+            }
+            console.log(actual);
+
+        }
+
+    }
+
+    const [touchStart, setTouchStart] = useState()
     const hanldeTouchstart = (e) => {
         setTouchStart(e.changedTouches[0].clientX)
     }
+
     const hanldeTouchEnd = (e) => {
-        console.log(touchStart -100 , e.changedTouches[0].clientX);
-        if(touchStart -100 > e.changedTouches[0].clientX){
-            console.log("adelante");
-        }else if(touchStart +100 < e.changedTouches[0].clientX){
-            console.log("atras");
+        // next
+        if (touchStart - 50 > e.changedTouches[0].clientX) {
+            let actual = current + 1
+            if (actual >= carrousel.children.length - 1) {
+                setCurrent(0)
+            }
+            setCurrent(actual)
+            hanldeNext(true, actual)
+        // previus
+        } else if (touchStart + 50 < e.changedTouches[0].clientX) {
+            let actual = current - 1
+            setCurrent(actual)
+            hanldeNext(false, actual)
         }
     }
 
-  
+
 
 
 
     return <div>
 
         <ul
-            onTouchStart={(e) => hanldeTouchstart(e)} 
-            onTouchEnd={(e) => hanldeTouchEnd(e)} 
+            onTouchStart={(e) => hanldeTouchstart(e)}
+            onTouchEnd={(e) => hanldeTouchEnd(e)}
 
             ref={refCarusel} className={styles.carrousel} >
             {img_webs.map((v, k) => (
-                <li className={styles.item} key={k}>
+                <li onClick={(e) => handleClick(e, k)} className={styles.item} key={k}>
                     <Card lg={v} />
                 </li>
             ))}
